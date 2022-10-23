a7dff1d88fb43a4b46f605a8a87e0bf0c6f59bfc
Zack Chen <zackchen5813@gmail.com>
2022-10-23 23:18:02 +1100

    init react portfolio

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <div className=" flex gap-8 justify-center m-9">
+      <button className="">About</button>
+      <button className="">Education</button>
+      <button className="">Skill</button>
+      <button className="">Experience</button>
+      <button className="">Contact</button>
+    </div>
+  )
+}
+export default Navbar
